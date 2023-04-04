746a4e15bff0cb4df8c800902d180f84e10e2225
Julia <julia.maliutsina@gmail.com>
2023-04-04 18:43:30 +0300

    toggle city data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 
 import getCitizens from './api/getCitizens';
+import getCity from './api/getCity';
 import './App.css';
 
 const App = () => {
   let [citizens, setCitizens] = useState([]);
+  let [cityData, setCityData] = useState({});
 
   useEffect(() => {
     getCitizens()
@@ -20,7 +22,38 @@ const App = () => {
       });
   }, []);
 
-  return <div className="App">{citizens[0] && citizens[0].name}</div>;
+  const showCityData = async (id) => {
+    getCity(id)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          console.log(response);
+        }
+      })
+      .then((data) => {
+        setCityData(data.city);
+      });
+  };
+  const hideCityData = () => {
+    setCityData({});
+  };
+
+  return (
+    <div className="App">
+      <div>{cityData._id}</div>
+      {citizens[0] &&
+        citizens.map((citizen) => (
+          <div
+            key={citizen._id}
+            onMouseEnter={() => showCityData(citizen.city_id)}
+            onMouseLeave={hideCityData}
+          >
+            {citizen.name}
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default App;
