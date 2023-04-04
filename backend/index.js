a17dc158ad82b4ef7eb592daa624ce229d494c3c
Julia <julia.maliutsina@gmail.com>
2023-04-04 16:14:45 +0300

    seed database

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
 import { cityRouter, citizenRouter } from './src/routes/index.js';
+import City from './src/db/models/city.js';
+import Citizen from './src/db/models/citizen.js';
+import { CITIZENS } from './src/constants/citizens.js';
+import { CITIES } from './src/constants/cities.js';
 
 dotenv.config();
 
@@ -27,6 +31,15 @@ const connectToDatabase = async () => {
   }
 };
 
+const seedDatabase = async () => {
+  await City.deleteMany();
+  await Citizen.deleteMany();
+  await City.insertMany(CITIES);
+  await Citizen.insertMany(CITIZENS);
+};
+
 app.listen(PORT, () => {
-  connectToDatabase().then(() => console.log(`Server started on port: ${PORT}`));
+  connectToDatabase()
+    .then(() => seedDatabase())
+    .then(() => console.log(`Server started on port: ${PORT}`));
 });
