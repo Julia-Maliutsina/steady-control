2126ad7d2d9a612620116e600e093279066d83c0
Julia <julia.maliutsina@gmail.com>
2023-04-05 22:13:34 +0300

    group search results

--- backend/src/controllers/citizensController.js
+++ backend/src/controllers/citizensController.js
@@ -3,7 +3,68 @@ import Citizen from '../db/models/citizen.js';
 const getAllCitizens = async (req, res) => {
   try {
     const citizens = await Citizen.find();
-    res.json({ citizens });
+
+    const addElement = (i, citizen) => {
+      let result = false;
+      if (i < citizen.groups.length) {
+        result = {
+          type: citizen.groups[i].type,
+          name: citizen.groups[i].name,
+        };
+        if (i !== citizen.groups.length - 1) {
+          result.elements = [addElement(i + 1, citizen)];
+        } else {
+          result.citizens = [{ _id: citizen._id, name: citizen.name, city_id: citizen.city_id }];
+        }
+      }
+      return result;
+    };
+
+    const checkGroup = (array, i, citizen) => {
+      const lastLevelGroup = i === citizen.groups.length - 1;
+      let elementIndex = array.findIndex(
+        (el) => el.name === citizen.groups[i].name && el.type === citizen.groups[i].type,
+      );
+      if (elementIndex < 0) {
+        let object = {
+          type: citizen.groups[i].type,
+          name: citizen.groups[i].name,
+        };
+        if (lastLevelGroup) {
+          const { _id, name, city_id } = citizen;
+          object.citizens = [{ _id, name, city_id }];
+        } else {
+          object.elements = [addElement(i + 1, citizen)];
+        }
+        array.push(object);
+        return { isElementAdded: true, array };
+      } else {
+        if (lastLevelGroup) {
+          const { _id, name, city_id } = citizen;
+          array[elementIndex].citizens
+            ? array[elementIndex].citizens.push({ _id, name, city_id })
+            : (array[elementIndex].citizens = [{ _id, name, city_id }]);
+          return { isElementAdded: true, array };
+        }
+        return { isElementAdded: false, array: array[elementIndex].elements };
+      }
+    };
+
+    let result = [];
+
+    citizens.forEach((citizen) => {
+      let arrayToCheck = result;
+      for (let i = 0; i < citizen.groups.length; i++) {
+        let { isElementAdded, array } = checkGroup(arrayToCheck, i, citizen);
+        arrayToCheck = array;
+        if (isElementAdded) {
+          break;
+        } else {
+          continue;
+        }
+      }
+    });
+    res.json({ result });
   } catch (e) {
     console.log(e);
   }
